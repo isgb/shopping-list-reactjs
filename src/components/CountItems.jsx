441b78b774624ec75862618ba96cc416de828cce
oscar <oscar.vasquez@zentro.com.mx>
2024-05-15 12:29:39 -0600

    debug

--- src/components/CountItems.jsx
+++ src/components/CountItems.jsx
@@ -1,6 +1,4 @@
 import React, { useContext } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle, faCheckCircle, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles.css';
 import { DataItemsContext } from '../context/DataItemsContext';
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,8 @@
-import React, { useContext } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircle, faCheckCircle, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import React, { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle, faCheckCircle, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { DataItemsContext } from '../context/DataItemsContext';
 
 export const Item = ({name, count, selected, index }) => 
