e1cd8093b39ab53e9aa134aa8eb7d04fd04d23ce
oscar <oscar.vasquez@zentro.com.mx>
2024-05-15 12:36:14 -0600

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,14 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Item } from './components/Item';
-import { useContext } from 'react';
-import { Input } from './components/Input';
-import { CountItems } from './components/CountItems';
-import { DataItemsContext } from './context/DataItemsContext';
+import { AppContainer } from './components/AppContainer';
 
 const App = () => {
 
-  const {items} = useContext(DataItemsContext);
 
   return (
     <>
-      <div className='app-container d-flex justify-content-center align-items-center'>
-
-        <div className='list-container container'>
-          <div className='row'>
-
-            <Input/>
-
-            {
-              items.map((item,index) => (
-                  <Item 
-                    key={index}
-                    index={index}
-                    name={item.itemName}
-                    count={item.count}
-                    selected={item.selected}
-                  /> 
-              ))
-            }
-
-           <CountItems/>
-
-          </div>
-
-        </div>
-      </div>
+     <AppContainer/>
     </>
   );
 }
